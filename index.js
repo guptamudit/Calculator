43541013c6f9cbb914472aea5b06db085a07baa3
Mudit <muditgupta1711@gmail.com>
2023-12-10 17:20:30 +0530

    added functionality

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+var result = document.getElementById("result");
 
+//display numbers in input field
+function display(number) {
+  result.value += number;
+}
+
+// function to calculate
+// the final result when clicking on "equal"
+function calc() {
+  var final_num = result.value;
+  var final_result = eval(final_num); // eval func to calculate the argument
+  result.value = final_result;
+}
+
+// function to clear the input field
+function clrsrc() {
+  result.value = "";
+}
+
+// function to delete a value from input field
+function del() {
+  result.value = result.value.slice(0, -1);
+}
